21b818160c76cb6c6cf54f674b232dcd5f75255a
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-08-12 20:35:54 +0530

    filtering

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,25 +1,59 @@
 import { useInventory } from "../main";
+import { inventoryConstants } from "../constants/inventory-constants";
 
 export const Filters = () => {
-  const { departments } = useInventory();
+  const {
+    inventory: { departmentFilter, lowStockFilter, sortFilter },
+    setInventory,
+    departments,
+  } = useInventory();
+  const { SET_DEPARTMENT_FILTER, SET_LOW_STOCK_FILTER, SET_SORT_FILTER } =
+    inventoryConstants;
 
   const displayDepartments = ["All Departments", ...departments];
-
   const sortTags = ["Name", "Price", "Stock"];
+
   return (
     <div className="flex items-center justify-between">
       <div className="text-xl font-bold">Products</div>
-      <select className="border-2 border-black px-5 py-2">
+      <select
+        className="border-2 border-black px-5 py-2"
+        defaultValue={departmentFilter}
+        onChange={(e) =>
+          setInventory({ type: SET_DEPARTMENT_FILTER, payload: e.target.value })
+        }
+      >
         {displayDepartments?.map((item, index) => (
-          <option key={index}>{item}</option>
+          <option key={index} value={item}>
+            {item}
+          </option>
         ))}
       </select>
       <label>
-        <input type="checkbox" /> Low Stock Items
+        <input
+          className="hover:cursor-pointer"
+          type="checkbox"
+          defaultChecked={lowStockFilter}
+          onChange={(e) =>
+            setInventory({
+              type: SET_LOW_STOCK_FILTER,
+              payload: e.target.checked,
+            })
+          }
+        />{" "}
+        Low Stock Items
       </label>
-      <select className="border-2 border-black px-5 py-2">
+      <select
+        className="border-2 border-black px-5 py-2"
+        defaultValue={sortFilter}
+        onChange={(e) =>
+          setInventory({ type: SET_SORT_FILTER, payload: e.target.value })
+        }
+      >
         {sortTags?.map((item, index) => (
-          <option key={index}>{item}</option>
+          <option key={index} value={item}>
+            {item}
+          </option>
         ))}
       </select>
       <button className="rounded-md bg-blue-500 px-5 py-2 text-white hover:cursor-pointer hover:bg-blue-400">
--- src/reducers/inventory-reducer.jsx
+++ src/reducers/inventory-reducer.jsx
@@ -1,12 +1,23 @@
 import { inventoryData } from "../data/inventory-data";
 import { inventoryConstants } from "../constants/inventory-constants";
 
-const { SET_INVENTORY } = inventoryConstants;
+const {
+  SET_INVENTORY,
+  SET_DEPARTMENT_FILTER,
+  SET_LOW_STOCK_FILTER,
+  SET_SORT_FILTER,
+} = inventoryConstants;
 
 export const inventoryReducer = (state, { type, payload }) => {
   switch (type) {
     case SET_INVENTORY:
       return { ...state, inventoryData: payload };
+    case SET_DEPARTMENT_FILTER:
+      return { ...state, departmentFilter: payload };
+    case SET_LOW_STOCK_FILTER:
+      return { ...state, lowStockFilter: payload };
+    case SET_SORT_FILTER:
+      return { ...state, sortFilter: payload };
     default:
       return state;
   }
