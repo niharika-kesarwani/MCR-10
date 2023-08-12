82e22d185c3459af16c307b38a7730f8e1360d30
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-08-12 21:28:25 +0530

    department routing

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import { Card } from "../components/Card";
 import { useInventory } from "../main";
+import { inventoryConstants } from "../constants/inventory-constants";
 
 export const Departments = () => {
-  const { departments } = useInventory();
+  const { setInventory, departments } = useInventory();
+  const navigate = useNavigate();
+  const { SET_DEPARTMENT_FILTER } = inventoryConstants;
 
   return (
     <div className="flex font-bold">
       {departments?.map((item, index) => (
-        <Card key={index}>{item}</Card>
+        <div
+          key={index}
+          onClick={() => {
+            setInventory({ type: SET_DEPARTMENT_FILTER, payload: item });
+            navigate("/products");
+          }}
+          className="hover:cursor-pointer"
+        >
+          <Card>{item}</Card>
+        </div>
       ))}
     </div>
   );
