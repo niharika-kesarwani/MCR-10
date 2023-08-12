16c7c4a7302b5c58f296b34e9380598677545e92
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-08-12 19:25:36 +0530

    departments page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export const Card = ({ children }) => {
   return (
-    <div className="m-10 h-min w-52 flex-wrap rounded-lg bg-gray-200 px-10 py-5">
+    <div className="m-10 h-min w-52 flex-wrap rounded-lg bg-gray-200 px-10 py-5 text-center">
       {children}
     </div>
   );
--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,3 +1,22 @@
+import { Card } from "../components/Card";
+import { useInventory } from "../main";
+
 export const Departments = () => {
-  return <div>Departments</div>;
+  const {
+    inventory: { inventoryData },
+  } = useInventory();
+
+  const departments = inventoryData?.reduce(
+    (final, { department }) =>
+      final.includes(department) ? final : [...final, department],
+    []
+  );
+
+  return (
+    <div className="flex font-bold">
+      {departments?.map((item, index) => (
+        <Card key={index}>{item}</Card>
+      ))}
+    </div>
+  );
 };
